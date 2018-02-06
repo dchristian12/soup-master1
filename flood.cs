bd5ff977614101c34ccda862ecb45d1e1ef1f5e6
dchristian12 <36180250+dchristian12@users.noreply.github.com>
2018-02-05 22:17:36 -0500

    Update flood.cs

--- flood.cs
+++ flood.cs
@@ -19,7 +19,7 @@ namespace CallBomber
     {
         public static string accountSid = "*********";
         public static string authtoken = "*********";
-        public static List<string> numbers = new List<string>(new string[] { "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", }); // Replace "" with phone numbers
+        public static List<string> numbers = new List<string>(new string[] { "+16785710343", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", "+10000000000", }); // Replace "" with phone numbers
         public static List<string> numbersInUse = new List<string>();
         public static string NumToCall = "";
         static void Main(string[] args)
